5d130ade42d640955d715f75e59f1bcf6433ab1e
poornajp <129258646+poornajp@users.noreply.github.com>
2023-08-21 10:05:24 +0530

    Update two.java

--- two.java
+++ two.java
@@ -2,7 +2,7 @@ public class two {
 
   public static void main(String[] args) {
     
-    int first = 5;
+    int first = 58;
     int second = 60;
     int sum = first + second;
     System.out.println(first + " + " + second + " = "  + sum);
