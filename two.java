2331c721518212495f5cc94a85b6e9660c5677ef
poorna <poorna123@gmail.com>
2023-08-21 09:59:10 +0530

    change1

---
+++ two.java
@@ -0,0 +1,10 @@
+public class two {
+
+  public static void main(String[] args) {
+    
+    int first = 45;
+    int second = 30;
+    int sum = first + second;
+    System.out.println(first + " + " + second + " = "  + sum);
+  }
+}
